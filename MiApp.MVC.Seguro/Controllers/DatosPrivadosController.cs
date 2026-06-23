74d5a40410d684713aca321704cedb1e5ad84534
Diego Trejo <diego_trejo@hotmail.com>
2026-06-23 12:14:24 -0500

    agregar Identity

---
+++ MiApp.MVC.Seguro/Controllers/DatosPrivadosController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiApp.MVC.Seguro.Controllers
+{
+    [Authorize]
+    public class DatosPrivadosController : Controller
+    {
+        // GET: DatosPrivadosController
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DatosPrivadosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DatosPrivadosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DatosPrivadosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DatosPrivadosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DatosPrivadosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DatosPrivadosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DatosPrivadosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
