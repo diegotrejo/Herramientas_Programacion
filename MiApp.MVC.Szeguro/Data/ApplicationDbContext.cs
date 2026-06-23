57e8b574d82da129532b9b9b4f1f78d4d3cc5e97
Diego Trejo <diego_trejo@hotmail.com>
2026-06-23 11:30:43 -0500

    proyecto mvc

---
+++ MiApp.MVC.Szeguro/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiApp.MVC.Szeguro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ MiApp.MVC.Szeguro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiApp.MVC.Szeguro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
